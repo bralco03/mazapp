2c846542ebdbf7fc47c73cdfa19ec5328d2e95f0
Bryan Correa <bralco03@hotmail.com>
2020-03-02 23:08:42 -0700

    Added google map from API however it is not yet fully functional due to billing issues.

---
+++ app.js
@@ -0,0 +1,12 @@
+// const apiKey = 'AIzaSyBb0BsKz11R1pHxTxNYjp7bfbHJi5sdLb8';
+
+function initMap(){
+   var options = {
+    zoom: 13,
+    center: {lat: 23.2329006, lng: -106.4061966}
+   };
+   var map = new google.maps.Map(document.getElementById('map-container'), options);
+   
+};
+
+
